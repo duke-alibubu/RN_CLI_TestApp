d32eef1ac9a6a0adea4e001e2642ac09d0e5ed9b
Trinh Dung <c170017@e.ntu.edu.sg>
2020-08-31 14:54:19 +0800

    trying file viewer for the first time

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import PdfScreen from './src/screens/PdfScreen';
+import FileViewerScreen from './src/screens/FileViewerScreen';
 
 const Stack = createStackNavigator();
 
 function Navigator() {
   return (
     <Stack.Navigator
-      initialRouteName="PDF"
+      initialRouteName="FileViewer"
       screenOptions={{ title: 'Testing Gesture Handlers' }}>
       <Stack.Screen name="PDF" component={PdfScreen} />
+      <Stack.Screen name="FileViewer" component={FileViewerScreen} />
     </Stack.Navigator>
   );
 }
