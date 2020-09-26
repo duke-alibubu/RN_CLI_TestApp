0d761b34c6912af1afb04272589c3f768076161a
Trinh Dung <c170017@e.ntu.edu.sg>
2020-09-26 21:50:46 +0800

    logic to conditionally display control is done

--- src/components/VideoComponent.js
+++ src/components/VideoComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, View, Dimensions, TouchableWithoutFeedback } from 'react-native';
 import Video from 'react-native-video';
 import { PLAYER_STATES } from 'react-native-media-controls';
@@ -6,6 +6,7 @@ import VideoPlayerControl from './VideoPlayerControl';
 import VideoProgressBar from './VideoProgressBar';
 
 const VideoComponent = ({ videoHeight, videoWidth }) => {
+    const SHOW_CONTROL_TIMEOUT = 2500;
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
     const [isFullScr, setIsFullScr] = useState(false);
@@ -17,6 +18,11 @@ const VideoComponent = ({ videoHeight, videoWidth }) => {
 
     const videoPlayerRef = useRef(React.createRef());
 
+    useEffect(() => {
+        setTimeout(() => {
+            setShowControls(false);
+        }, SHOW_CONTROL_TIMEOUT);
+    }, [])
     const doSeek = seek => {
         //Handler for change in seekbar
         videoPlayerRef.current.seek(seek);
@@ -25,11 +31,15 @@ const VideoComponent = ({ videoHeight, videoWidth }) => {
     const pauseVideo = () => {
         setPaused(true);
         setPlayerState(PLAYER_STATES.PAUSED)
+        setShowControls(true);
     };
 
     const resumeVideo = () => {
         setPaused(false);
         setPlayerState(PLAYER_STATES.PLAYING)
+        setTimeout(() => {
+            setShowControls(false);
+        }, SHOW_CONTROL_TIMEOUT)
     }
 
     const onReplay = async () => {
@@ -57,12 +67,21 @@ const VideoComponent = ({ videoHeight, videoWidth }) => {
     const onEnd = () => {
         setPlayerState(PLAYER_STATES.ENDED);
         setPaused(true);
+        setShowControls(true);
     }
 
     const onError = () => alert('Oh! ', error);
 
     const toggleShowControls = () => {
-        setShowControls(!showControls);
+        if (showControls) {
+            setShowControls(false);
+        }
+        else {
+            setShowControls(true);
+            setTimeout(() => {
+                setShowControls(false);
+            }, SHOW_CONTROL_TIMEOUT);
+        }
     }
 
     const styles = StyleSheet.create({
