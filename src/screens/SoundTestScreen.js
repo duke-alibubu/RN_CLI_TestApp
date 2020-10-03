dcc76540bf8a56969bf3faaafe6dac843af49cf5
Trinh Dung <c170017@e.ntu.edu.sg>
2020-10-03 12:24:48 +0800

    successfully set State of sound player

--- src/screens/SoundTestScreen.js
+++ src/screens/SoundTestScreen.js
@@ -5,20 +5,36 @@ import { SoundPause, SoundPlay } from '../../assets/sound_icons';
 
 const SoundTestScreen = () => {
     const [isPlaying, setIsPlaying] = useState(false);
-    const soundRef = useRef(new Sound("http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3"));
-    const playSound = () => {
+    const soundRef = useRef(new Sound("https://dl.espressif.com/dl/audio/gs-16b-2c-44100hz.mp3", '', (error) => {
+        if (error) {
+            console.log('failed to load the sound', error);
+            Alert.alert('Notice', 'audio file error. (Error code : 1)');
+            this.setState({ playState: 'paused' });
+        }
+    }));
+
+    const playOrPauseSound = () => {
         if (isPlaying) {
             soundRef.current.pause();
         }
         else {
-            soundRef.current.play();
+            soundRef.current.play(onFinish);
         }
         setIsPlaying(!isPlaying);
     };
 
+    const onFinish = (success) => {
+        if (success) {
+            console.log('successfully finished playing');
+        } else {
+            console.log('playback failed due to audio decoding errors');
+            Alert.alert('Notice', 'audio file error. (Error code : 2)');
+        }
+        setIsPlaying(false);
+    }
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={playSound}>
+            <TouchableOpacity onPress={playOrPauseSound}>
                 {isPlaying ? <SoundPause /> : <SoundPlay />}
             </TouchableOpacity>
         </View>
