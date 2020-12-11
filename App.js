982b1a628ccb20fd5ab00f1ac76f9284a77abf3e
Trinh Dung <c170017@e.ntu.edu.sg>
2020-12-11 17:00:56 +0800

    experimenting drawer navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import PdfScreen from './src/screens/PdfScreen';
 import FileViewerScreen from './src/screens/FileViewerScreen';
 import VideoScreen from './src/screens/VideoScreen';
@@ -12,11 +13,12 @@ import SoundTestScreen from './src/screens/SoundTestScreen';
 import SinglePageScreen from './src/screens/SinglePageScreen';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function Navigator() {
+function StackNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="SinglePage"
+      initialRouteName="VideoPlayerScreen"
       screenOptions={{ title: 'Testing Gesture Handlers' }}>
       <Stack.Screen name="PDF" component={PdfScreen} />
       <Stack.Screen name="FileViewer" component={FileViewerScreen} />
@@ -30,10 +32,20 @@ function Navigator() {
   );
 }
 
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Week 1" component={SinglePageScreen} />
+      <Drawer.Screen name="Week 2" component={SinglePageScreen} />
+      <Drawer.Screen name="Nested" component={StackNavigator} />
+    </Drawer.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Navigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
